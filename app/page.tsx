1441e50f983ec7a67d98355bf49d7d70a2288a77
古賀龍太 <kogaryuuta@kogaryuutanoMac-mini.local>
2024-06-04 23:14:18 +0900

    feat: supabaseからデータの取得をして表示する実装

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
-import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
-  const supabase = createServerComponentClient({cookies});
+export default async function Home() {
+  const supabase = createServerComponentClient({ cookies });
+  const { data: lessons } = await supabase.from("lesson").select("*");
+  console.log(lessons);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
+    <main className="w-full max-w-3xl mx-auto my-16 px-2">
+      {lessons?.map((lesson) => (
+        <Link href={`/${lesson.id}`} key={lesson.id}>
+          {lesson.title}
+        </Link>
+      ))}
     </main>
   );
 }
