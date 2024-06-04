1f737a18b16cbe7e40ac644c13d7589984502970
古賀龍太 <kogaryuuta@kogaryuutanoMac-mini.local>
2024-06-04 23:27:58 +0900

    feat: shadcn/uiのCardコンポーネントの導入とインスタンス化のリファクタリング

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,38 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import Link from "next/link";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+const supabase = createServerComponentClient({ cookies });
+const getAllLessons = async () => {
+  const { data: lessons } = await supabase.from("lesson").select("*");
+  return lessons;
+};
 
 export default async function Home() {
-  const supabase = createServerComponentClient({ cookies });
-  const { data: lessons } = await supabase.from("lesson").select("*");
-  console.log(lessons);
+  const lessons = await getAllLessons();
   return (
     <main className="w-full max-w-3xl mx-auto my-16 px-2">
-      {lessons?.map((lesson) => (
-        <Link href={`/${lesson.id}`} key={lesson.id}>
-          {lesson.title}
-        </Link>
-      ))}
+      <div className="flex flex-col gap-3">
+        {lessons?.map((lesson) => (
+          <Link href={`/${lesson.id}`} key={lesson.id}>
+            <Card>
+              <CardHeader>
+                <CardTitle> {lesson.title}</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p>{lesson.description}</p>
+              </CardContent>
+            </Card>
+          </Link>
+        ))}
+      </div>
     </main>
   );
 }
